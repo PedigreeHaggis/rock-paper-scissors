a5f5ac6cf4f71680c4e92044845267a8d8c3978c
Gary Taylor <garycharlestaylor@outlook.com>
2022-08-01 23:00:01 +0100

    finished logic need to clean

--- resources/js/script.js
+++ resources/js/script.js
@@ -1,10 +1,56 @@
+const rockButton = document.querySelector('.rock-button');
+const paperButton = document.querySelector('.paper-button');
+const scissorButton = document.querySelector('.scissor-button');
+const winnerMessage = document.querySelector('.winner-message');
+const userScoreNumber = document.querySelector('.user-score-number');
+const computerScoreNumber = document.querySelector('.computer-score-number');
+
+let userScore = 0;
+let computerScore = 0;
+
+userScoreNumber.textContent = userScore;
+computerScoreNumber.textContent = computerScore;
+
 const rules = {
     rock:'scissors',
     paper: 'rock',
     scissors: 'paper',
 }
 
-const playRound = (p1, p2) => p1.toLowerCase() === rules[p2] ? 'Winner' : p1.toLowerCase() === p2 ? 'Draw' : 'Loser'; 
+
+
+function playRound(userChoice, computerChoice){
+    const winMessage = 'You Won';
+    const loseMessage = 'You Lost';
+    const drawMessage = 'Its a Draw';
+
+
+
+    if(userChoice.toLowerCase() === rules[computerChoice]){
+        winnerMessage.textContent = winMessage
+        console.log(winMessage)
+        userScore++;
+        userScoreNumber.textContent = userScore
+    } else if (userChoice.toLowerCase() === computerChoice){
+        winnerMessage.textContent = drawMessage
+        console.log(drawMessage);
+    } else{
+        winnerMessage.textContent = loseMessage
+        console.log(loseMessage)
+        computerScore++
+        computerScoreNumber.textContent = computerScore
+    }
+
+    if(userScore === 5 ){
+        userScore = 0;
+        computerScore = 0;
+        winnerMessage.textContent = "You won the Game";
+    } else if(computerScore === 5){
+        userScore = 0;
+        computerScore = 0
+        winnerMessage.textContent = "The computer won the game!";
+    }
+}
 
 function getComputerChoice(){
     const randomNumber = Math.floor(Math.random() * 3);
@@ -12,42 +58,24 @@ function getComputerChoice(){
     switch(randomNumber){
         case 0:
             return 'rock';
-            break;
         case 1:
             return 'scissors';
-            break;
         case 2:
             return 'paper';
-            break;
         default:
             break;
     }
 }
 
-function game(){
-    const winMessage = 'You Won';
-    const loseMessage = 'You Lost';
-    const drawMessage = 'Its a Draw';
+winnerMessage.textContent = ''
 
-    for(let i = 0; i < 5; i++){
-        // let userChoice = prompt("enter your choice:\nRock|Paper|Scissors");
-        switch(playRound(userChoice, getComputerChoice())){
-            case 'Winner':
-                alert(winMessage)
-                console.log(winMessage);
-                break;
-            case 'Loser':
-                alert(loseMessage)
-                console.log(loseMessage)
-                break;
-            case 'Draw':
-                alert(drawMessage);
-                console.log(drawMessage);
-                break;
-            default:
-                break;
-        }
-    }
-}
+rockButton.addEventListener('click', function() {
+    playRound('rock', getComputerChoice());
+});
+paperButton.addEventListener('click', function(){
+    playRound('paper', getComputerChoice())
+})
+scissorButton.addEventListener('click', function(){
+    playRound('scissors', getComputerChoice())
+});
 
-game()
