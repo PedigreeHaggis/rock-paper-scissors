d3be8cd9ff9b4451220a807a6de217802c84790b
Gary Taylor <garycharlestaylor@outlook.com>
2022-08-02 15:17:44 +0100

    finished ui and logic

--- resources/js/script.js
+++ resources/js/script.js
@@ -1,3 +1,7 @@
+//////////////////////////////////
+// Query Selectors & Variables //
+////////////////////////////////
+
 const rockButton = document.querySelector('.rock-button');
 const paperButton = document.querySelector('.paper-button');
 const scissorButton = document.querySelector('.scissor-button');
@@ -8,29 +12,25 @@ const computerScoreNumber = document.querySelector('.computer-score-number');
 let userScore = 0;
 let computerScore = 0;
 
-userScoreNumber.textContent = userScore;
-computerScoreNumber.textContent = computerScore;
-
 const rules = {
     rock:'scissors',
     paper: 'rock',
     scissors: 'paper',
 }
 
-
+///////////////////////////////
+// FUNCTIONS                //
+/////////////////////////////
 
 function playRound(userChoice, computerChoice){
-    const winMessage = 'You Won';
-    const loseMessage = 'You Lost';
-    const drawMessage = 'Its a Draw';
-
-
+    const winMessage = 'You Won this Round';
+    const loseMessage = 'You Lost this Round';
+    const drawMessage = 'Its a Draw!';
 
     if(userChoice.toLowerCase() === rules[computerChoice]){
         winnerMessage.textContent = winMessage
         console.log(winMessage)
         userScore++;
-        userScoreNumber.textContent = userScore
     } else if (userChoice.toLowerCase() === computerChoice){
         winnerMessage.textContent = drawMessage
         console.log(drawMessage);
@@ -38,17 +38,6 @@ function playRound(userChoice, computerChoice){
         winnerMessage.textContent = loseMessage
         console.log(loseMessage)
         computerScore++
-        computerScoreNumber.textContent = computerScore
-    }
-
-    if(userScore === 5 ){
-        userScore = 0;
-        computerScore = 0;
-        winnerMessage.textContent = "You won the Game";
-    } else if(computerScore === 5){
-        userScore = 0;
-        computerScore = 0
-        winnerMessage.textContent = "The computer won the game!";
     }
 }
 
@@ -67,15 +56,41 @@ function getComputerChoice(){
     }
 }
 
-winnerMessage.textContent = ''
+function resetGame(){
+    if(userScore === 5 ){
+        userScore = 0;
+        computerScore = 0;
+        winnerMessage.textContent = "You won the Game";
+    } else if(computerScore === 5){
+        userScore = 0;
+        computerScore = 0
+        winnerMessage.textContent = "The computer won the game!";
+    }
+}
+
+/////////////////////////////////
+// Event Listeners & Main Code//
+///////////////////////////////
+
+winnerMessage.style.textAlign = 'center'
+computerScoreNumber.textContent = userScore;
+userScoreNumber.textContent =computerScore;
 
 rockButton.addEventListener('click', function() {
     playRound('rock', getComputerChoice());
+    resetGame();
+    userScoreNumber.textContent = userScore;
+    computerScoreNumber.textContent = computerScore;
 });
 paperButton.addEventListener('click', function(){
-    playRound('paper', getComputerChoice())
+    playRound('paper', getComputerChoice());
+    resetGame();
+    userScoreNumber.textContent = userScore;
+    computerScoreNumber.textContent = computerScore;
 })
 scissorButton.addEventListener('click', function(){
-    playRound('scissors', getComputerChoice())
+    playRound('scissors', getComputerChoice());
+    resetGame();
+    userScoreNumber.textContent = userScore;
+    computerScoreNumber.textContent = computerScore;
 });
-
